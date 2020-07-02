8b56250d09340c2de1a9dde612656fde641f51bf
Kristian Hamre-Os <codedevstem@gmail.com>
2020-07-02 12:43:25 +0200

    initialized with server with /register endpoint

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const admin = require('firebase-admin')
+const serviceAccount = require("./keys/test-app-api-key.json")
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.post('/register', (req, res) => {
+    admin.auth().createUser({
+        displayName: req.body.username,
+        email: req.body.email,
+        emailVerified: false,
+        password: req.body.password,
+        disabled: false
+    })
+        .then(userRecord => {
+            console.log(`Successfully created new user: ${userRecord.uid}`);
+            res.json(userRecord)
+        })
+        .catch(error => {
+            console.log(`Error creating new user ${error}`)
+            res.status(500)
+        })
+});
+
+app.post('/login', (req, res) => {
+    admin.auth().get
+})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
